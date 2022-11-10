7dd3d20de9acdeae3985dcaf6eff61ef0ced54b3
Jesper Sporron <jesper.sporron@gmail.com>
2022-11-10 03:08:21 +0100

    feat(windowing): add windowing support

---
+++ src/JXS.Graphics.Core/Camera.cs
@@ -0,0 +1,87 @@
+﻿using OpenTK.Mathematics;
+
+namespace JXS.Graphics.Core;
+
+public abstract class Camera
+{
+	private const float REALLY_SMALL = 0.0000001f;
+
+	protected Camera(float worldWidth, float worldHeight)
+	{
+		WorldSize = new Vector2(worldWidth, worldHeight);
+	}
+
+	public Vector3 Position { get; set; }
+	public Vector3 Forward { get; set; } = -Vector3.UnitZ;
+	public Vector3 Up { get; set; } = Vector3.UnitY;
+	public Vector3 Right => Vector3.Cross(Forward, Up);
+
+	public Vector2 WindowSize { get; private set; }
+	public Vector2 WorldSize { get; set; }
+
+	public float NearClippingPlane { get; set; } = 1;
+	public float FarClippingPlane { get; set; } = 100;
+
+	public Matrix4 Combined => Projection * View;
+
+	public abstract Matrix4 Projection { get; }
+	public abstract Matrix4 View { get; }
+	
+	protected abstract void UpdateViewport(int windowWidth, int windowHeight);
+
+	public void Update(int newWidth, int newHeight)
+	{
+		WindowSize = new Vector2(newWidth, newHeight);
+	}
+
+	public void LookAt(Vector3 point)
+	{
+		var newForward = Vector3.Normalize(point - Position);
+		if (newForward.LengthSquared < REALLY_SMALL)
+		{
+			return;
+		}
+
+		var dot = Vector3.Dot(newForward, Up);
+		if (MathF.Abs(dot - 1) < REALLY_SMALL)
+		{
+			Up = -Forward;
+		}
+		else if (MathF.Abs(dot + 1) < REALLY_SMALL)
+		{
+			Up = Forward;
+		}
+
+		Forward = newForward;
+		NormalizeUp();
+	}
+
+	public void NormalizeUp()
+	{
+		Up = Vector3.Normalize(Vector3.Cross(Right, Forward));
+	}
+
+	public void Rotate(Vector3 axis, float angleRadians)
+	{
+		Forward = Vector3.Transform(Forward, Quaternion.FromAxisAngle(axis, angleRadians));
+		Up = Vector3.Transform(Up, Quaternion.FromAxisAngle(axis, angleRadians)).Normalized();
+	}
+
+	public void RotateAround(Vector3 point, Vector3 axis, float angleRadians)
+	{
+		var offset = point - Position;
+		Position += offset;
+		Rotate(axis, angleRadians);
+		Position -= Vector3.Transform(offset, Quaternion.FromAxisAngle(axis, angleRadians));
+	}
+
+	// public Ray ScreenPointToRay(Vector2 point)
+	// {
+	// 	var nearPoint =
+	// 		latestViewport.Unproject(point.AsXY(latestViewport.MinDepth), Projection, View, Matrix4.Identity);
+	// 	var farPoint =
+	// 		latestViewport.Unproject(point.AsXY(latestViewport.MaxDepth), Projection, View, Matrix4.Identity);
+	// 	var direction = (farPoint - nearPoint).Normalized();
+	// 	return new Ray(nearPoint, direction);
+	// }
+}
---
+++ src/JXS.Graphics.Core/OrthographicCamera.cs
@@ -0,0 +1,63 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace JXS.Graphics.Core;
+
+public class OrthographicCamera : Camera
+{
+	private readonly float baseWorldWidth;
+	private readonly float baseWorldHeight;
+
+	public OrthographicCamera(float worldWidth, float worldHeight) : base(worldWidth, worldHeight)
+	{
+		baseWorldWidth = worldWidth;
+		baseWorldHeight = worldHeight;
+	}
+
+	public override Matrix4 Projection =>
+		Matrix4.CreateOrthographic(WorldSize.X, WorldSize.Y, NearClippingPlane, FarClippingPlane);
+
+	public override Matrix4 View => Matrix4.LookAt(Position, Position + Forward, Up);
+
+	protected override void UpdateViewport(int screenWidth, int screenHeight)
+	{
+		var newWorldWidth = baseWorldWidth;
+		var newWorldHeight = baseWorldHeight;
+		var scaled = Scale(newWorldWidth, newWorldHeight, screenWidth, screenHeight);
+		var viewportWidth = MathF.Round(scaled.X);
+		var viewportHeight = MathF.Round(scaled.Y);
+
+		if (viewportWidth < screenWidth)
+		{
+			var toViewportSpace = viewportHeight / newWorldHeight;
+			var toWorldSpace = newWorldHeight / viewportHeight;
+			var lengthen = (screenWidth - viewportWidth) * toWorldSpace;
+			newWorldWidth += lengthen;
+			viewportWidth += MathF.Round(lengthen * toViewportSpace);
+		}
+		else if (viewportHeight < screenHeight)
+		{
+			var toViewportSpace = viewportWidth / newWorldWidth;
+			var toWorldSpace = newWorldWidth / viewportWidth;
+			var lengthen = (screenHeight - viewportHeight) * toWorldSpace;
+			newWorldHeight += lengthen;
+			viewportHeight += MathF.Round(lengthen * toViewportSpace);
+		}
+
+		WorldSize = new Vector2(newWorldWidth, newWorldHeight);
+		GL.Viewport(
+			(int)(screenWidth - viewportWidth) / 2,
+			(int)(screenHeight - viewportHeight) / 2,
+			(int)viewportWidth,
+			(int)viewportHeight
+		);
+	}
+
+	private static Vector2 Scale(float sourceWidth, float sourceHeight, float targetWidth, float targetHeight)
+	{
+		var targetRatio = targetHeight / targetWidth;
+		var sourceRatio = sourceHeight / sourceWidth;
+		var scale = targetRatio > sourceRatio ? targetWidth / sourceWidth : targetHeight / sourceHeight;
+		return new Vector2(sourceWidth, sourceHeight) * scale;
+	}
+}
