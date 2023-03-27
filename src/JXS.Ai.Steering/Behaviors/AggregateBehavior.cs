c8519eb15ff4c7c93e5dc6e834d2b96df3c4ea89
Jesper Sporron <jesper.sporron@gmail.com>
2023-03-27 16:02:23 +0200

    feat(ai): add Seek steering behavior

---
+++ src/JXS.Ai.Steering/Behaviors/AggregateBehavior.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace JXS.Ai.Steering.Behaviors;
+
+[PublicAPI]
+public class AggregateBehavior<TSteerable> : ISteeringBehavior<TSteerable> where TSteerable : ISteerable
+{
+	private readonly List<ISteeringBehavior<TSteerable>> steeringBehaviors;
+
+	public AggregateBehavior()
+	{
+		steeringBehaviors = new List<ISteeringBehavior<TSteerable>>();
+	}
+
+	public SteeringDelta Calculate(in TSteerable steerable)
+	{
+		var result = new SteeringDelta();
+		// ReSharper disable once ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
+		foreach (var steeringBehavior in steeringBehaviors)
+		{
+			result += steeringBehavior.Calculate(steerable);
+		}
+
+		return result;
+	}
+
+	public void Add(ISteeringBehavior<TSteerable> item) => steeringBehaviors.Add(item);
+
+	public bool Remove(ISteeringBehavior<TSteerable> item) => steeringBehaviors.Remove(item);
+}
