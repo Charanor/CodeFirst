b0f9597148930c4029448948a94f4de3d37900d8
Jesper Sporron <jesper.sporron@gmail.com>
2023-04-16 10:19:44 +0200

    feat(ecs): add entity query support

---
+++ src/JXS.Ecs.Core/Utilities/EntityQueryExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace JXS.Ecs.Core.Utilities;
+
+public static class EntityQueryExtensions
+{
+	/// <inheritdoc cref="EntityQuery" />
+	public static EntityQuery QueryEntities(this World world) => new(world);
+
+	/// <summary>
+	///     Exclude <paramref name="entityToExclude" /> from the results of this query.
+	/// </summary>
+	/// <param name="query">the query</param>
+	/// <param name="entityToExclude">the entity to exclude</param>
+	/// <returns>the query, for chaining</returns>
+	public static EntityQuery Exclude(this EntityQuery query, Entity entityToExclude) => query
+		.ThatMatch((_, entity) => entity != entityToExclude);
+}
