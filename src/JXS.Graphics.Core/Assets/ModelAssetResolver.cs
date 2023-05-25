09dfd37bfad0aad6e4ef6cea68f2924424c42399
Jesper Sporron <jesper.sporron@gmail.com>
2023-05-25 19:29:53 +0200

    feat(graphics): add support for vertex colors in model loader

--- src/JXS.Graphics.Core/Assets/ModelAssetResolver.cs
+++ src/JXS.Graphics.Core/Assets/ModelAssetResolver.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Assimp;
 using JXS.AssetManagement;
 using JXS.FileSystem;
+using JXS.Graphics.Core.Utils;
 using JXS.Utils;
 using Microsoft.VisualBasic.FileIO;
 using OpenTK.Mathematics;
@@ -14,7 +15,7 @@ public class ModelAssetResolver : IAssetResolver
 {
 	private const int INDEX_SIZE = sizeof(int) + sizeof(char); // We add sizeof(char) because of the value separator
 
-	private const int FLOATS_PER_VERTEX = 3 + 3 + 2; // Position + normal + texCoord
+	private const int FLOATS_PER_VERTEX = 3 + 3 + 4 + 2; // Position + normal + color (rgba) + texCoord
 	private const int VERTEX_SIZE = sizeof(float) * FLOATS_PER_VERTEX + sizeof(char);
 
 	private const string PACKAGED_MODEL_EXTENSION = ".csm";
@@ -111,8 +112,10 @@ public class ModelAssetResolver : IAssetResolver
 			{
 				var position = new Vector3(ReadFloat(ref dataIdx), ReadFloat(ref dataIdx), ReadFloat(ref dataIdx));
 				var normal = new Vector3(ReadFloat(ref dataIdx), ReadFloat(ref dataIdx), ReadFloat(ref dataIdx));
+				var color = new Vector4(ReadFloat(ref dataIdx), ReadFloat(ref dataIdx), ReadFloat(ref dataIdx),
+					ReadFloat(ref dataIdx)).ToColor4<Rgba>();
 				var texCoord = new Vector2(ReadFloat(ref dataIdx), ReadFloat(ref dataIdx));
-				currentVertices.Add(new Mesh.Vertex(position, normal, texCoord));
+				currentVertices.Add(new Mesh.Vertex(position, normal, color, texCoord));
 			}
 
 			meshes.Add(MainThread.Post(() => new Mesh(currentVertices, currentIndices, materialProvider())).Result);
@@ -144,6 +147,7 @@ public class ModelAssetResolver : IAssetResolver
 			Mesh.Vertex CreateForIndex(int index) => new(
 				ToOpenTK(sceneMesh.Vertices[index]),
 				ToOpenTK(sceneMesh.Normals[index]),
+				ToOpenTK(sceneMesh.VertexColorChannels[0][index]),
 				ToOpenTK(sceneMesh.TextureCoordinateChannels[0][index]).Xy
 			);
 		}
@@ -167,7 +171,7 @@ public class ModelAssetResolver : IAssetResolver
 				Int(index);
 			}
 
-			foreach (var ((pX, pY, pZ), (nX, nY, nZ), (u, v)) in vertices)
+			foreach (var ((pX, pY, pZ), (nX, nY, nZ), (r, g, b, a), (u, v)) in vertices)
 			{
 				Float(pX);
 				Float(pY);
@@ -175,6 +179,10 @@ public class ModelAssetResolver : IAssetResolver
 				Float(nX);
 				Float(nY);
 				Float(nZ);
+				Float(r);
+				Float(g);
+				Float(b);
+				Float(a);
 				Float(u);
 				Float(v);
 			}
@@ -216,9 +224,12 @@ public class ModelAssetResolver : IAssetResolver
 			{
 				var texCoordChannel = sceneMesh.TextureCoordinateChannels[0];
 				var hasTexCoords = texCoordChannel.Any();
+				var colorChannel = sceneMesh.VertexColorChannels[0];
+				var hasColor = colorChannel.Any();
 				return new Mesh.Vertex(
 					ToOpenTK(sceneMesh.Vertices[index]),
 					ToOpenTK(sceneMesh.Normals[index]),
+					hasColor ? ToOpenTK(colorChannel[index]) : Color4.White,
 					hasTexCoords ? ToOpenTK(texCoordChannel[index]).Xy : Vector2.Zero
 				);
 			}
@@ -241,7 +252,7 @@ public class ModelAssetResolver : IAssetResolver
 				output.Write((uint)index);
 			}
 
-			foreach (var ((pX, pY, pZ), (nX, nY, nZ), (u, v)) in vertices)
+			foreach (var ((pX, pY, pZ), (nX, nY, nZ), (r, g, b, a), (u, v)) in vertices)
 			{
 				output.Write(pX);
 				output.Write(pY);
@@ -249,6 +260,10 @@ public class ModelAssetResolver : IAssetResolver
 				output.Write(nX);
 				output.Write(nY);
 				output.Write(nZ);
+				output.Write(r);
+				output.Write(g);
+				output.Write(b);
+				output.Write(a);
 				output.Write(u);
 				output.Write(v);
 			}
@@ -280,10 +295,11 @@ public class ModelAssetResolver : IAssetResolver
 
 			for (var i = 0; i < meshVertexCount; i++)
 			{
-				var position = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
-				var normal = new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
-				var texCoord = new Vector2(reader.ReadSingle(), reader.ReadSingle());
-				currentVertices.Add(new Mesh.Vertex(position, normal, texCoord));
+				var position = ReadVector3();
+				var normal = ReadVector3();
+				var color4 = ReadVector4().ToColor4<Rgba>();
+				var texCoord = ReadVector2();
+				currentVertices.Add(new Mesh.Vertex(position, normal, color4, texCoord));
 			}
 
 			var meshCreator = MainThread.Post(() => new Mesh(currentVertices, currentIndices, materialProvider()));
@@ -293,9 +309,19 @@ public class ModelAssetResolver : IAssetResolver
 
 		model = new Model(meshes);
 		return true;
+
+		Vector2 ReadVector2() => new(reader.ReadSingle(), reader.ReadSingle());
+
+		Vector3 ReadVector3() => new(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+		Vector4 ReadVector4() =>
+			new(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
 	}
 
 	private static Vector3 ToOpenTK(Vector3D assimpVector) => new(assimpVector.X, assimpVector.Y, assimpVector.Z);
+
+	private static Color4<Rgba> ToOpenTK(Color4D assimpColor) =>
+		new(assimpColor.R, assimpColor.G, assimpColor.B, assimpColor.A);
 
 	private record PackagedMeshIntermediateFormat(int[] Indices, Mesh.Vertex[] Vertices);
 }
