6e14a9cd94cb444d9d934cf7c42a0d1f47a0a7ad
Jesper Sporron <jesper.sporron@gmail.com>
2023-12-28 07:26:04 +0100

    feat(utils): add argument-less event handler

--- src/CodeFirst/Utils/Events/EventHandler.cs
+++ src/CodeFirst/Utils/Events/EventHandler.cs
@@ -1,3 +1,4 @@
 namespace CodeFirst.Utils.Events;
 
 public delegate void EventHandler<in TSender, in TArgs>(TSender sender, TArgs e);
+public delegate void EventHandler<in TSender>(TSender sender);
