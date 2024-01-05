7dd6a98d1913a821181ac18858466ce2fc82241f
Jesper Sporron <jesper.sporron@gmail.com>
2024-01-05 23:12:08 +0100

    Remove unused window functions

--- src/CodeFirst/Gui/IGraphicsProvider.cs
+++ src/CodeFirst/Gui/IGraphicsProvider.cs
@@ -19,48 +19,6 @@ public interface IGraphicsProvider
 		float borderBottomLeftRadius = default,
 		float borderBottomRightRadius = default);
 
-	void DrawWindow(
-		Box2 bounds,
-		float borderSize,
-		Texture2D? topLeft,
-		Texture2D? topEdge,
-		Texture2D? topRight,
-		Texture2D? bottomLeft,
-		Texture2D? bottomEdge,
-		Texture2D? bottomRight,
-		Texture2D? leftEdge,
-		Texture2D? rightEdge,
-		Texture2D? fill,
-		bool tileFill = false,
-		bool flipEdges = false,
-		bool flipCorners = false
-	);
-
-	void DrawWindow(
-		Box2 bounds,
-		float borderSize,
-		Texture2D? topLeft,
-		Texture2D? topRight,
-		Texture2D? bottomLeft,
-		Texture2D? bottomRight,
-		Texture2D? edges,
-		Texture2D? fill,
-		bool tileFill = false,
-		bool flipEdges = false,
-		bool flipCorners = false
-	);
-
-	void DrawWindow(
-		Box2 bounds,
-		float borderSize,
-		Texture2D? corners,
-		Texture2D? edges,
-		Texture2D? fill,
-		bool tileFill = false,
-		bool flipEdges = false,
-		bool flipCorners = false
-	);
-
 	void DrawImage(Box2 bounds, Texture2D texture,
 		float borderTopLeftRadius = default,
 		float borderTopRightRadius = default,
