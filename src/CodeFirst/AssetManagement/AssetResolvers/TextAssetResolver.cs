891e9639c7acbe1dcd2ebde899c9875321d20500
Jesper Sporron <jesper.sporron@gmail.com>
2024-11-30 09:38:46 +0100

    feat(assets): remove need of .txt extension for TextAssetResolver

--- src/CodeFirst/AssetManagement/AssetResolvers/TextAssetResolver.cs
+++ src/CodeFirst/AssetManagement/AssetResolvers/TextAssetResolver.cs
@@ -5,19 +5,11 @@ namespace CodeFirst.AssetManagement.AssetResolvers;
 
 public class TextAssetResolver : IAssetResolver
 {
-	private const string TEXT_FILE_EXTENSION = ".txt";
-
 	public bool CanLoadAssetOfType(Type type) => type == typeof(string);
 
 	public bool TryLoadAsset(FileHandle fileHandle, [NotNullWhen(true)] out object? asset)
 	{
 		if (fileHandle.Type != FileType.File)
-		{
-			asset = default;
-			return false;
-		}
-
-		if (Path.GetExtension(fileHandle.FilePath) != TEXT_FILE_EXTENSION)
 		{
 			asset = default;
 			return false;
